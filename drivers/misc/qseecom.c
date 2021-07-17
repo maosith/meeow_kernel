9e5827561957912e94a59816bd1e723b2082e71b
v4lkyr <valkyr23@gmail.com>
2021-07-18 00:17:34 +0800

    qseecom: Fix memcpy linking error with GCC LTO
    This commit fixes below error:
    
    In function 'memcpy',
        inlined from 'qseecom_scm_call2' at ../drivers/misc/qseecom.c:891:4,
        inlined from 'qseecom_scm_call' at ../drivers/misc/qseecom.c:1175:9,
        inlined from '__qseecom_unregister_listener' at ../drivers/misc/qseecom.c:1613:8,
        inlined from '__qseecom_processing_pending_lsnr_unregister' at ../drivers/misc/qseecom.c:1715:11,
        inlined from '__qseecom_unregister_listener_kthread_func' at ../drivers/misc/qseecom.c:1752:3:
    ../include/linux/string.h:375:25: error: call to '__read_overflow2' declared with attribute error: detected read beyond size of object passed as 2nd parameter
      375 |                         __read_overflow2();
          |                         ^
    make[2]: *** [/tmp/ccS852MC.mk:167: /tmp/cc0v2DzD.ltrans55.ltrans.o] Error 1
    make[2]: *** Waiting for unfinished jobs....
    lto-wrapper: fatal error: make returned 2 exit status
    compilation terminated.
    
    Big thanks to jebaitedneko
    
    Signed-off-by: v4lkyr <valkyr23@gmail.com>
    Change-Id: I725728fb4cb481c6d9d7aebfebeef7659718fb5c
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- drivers/misc/qseecom.c
+++ drivers/misc/qseecom.c
@@ -1026,7 +1026,7 @@ static int qseecom_scm_call2(uint32_t svc_id, uint32_t tz_cmd_id,
 			if (!tzbuf)
 				return -ENOMEM;
 			memset(tzbuf, 0, tzbuflen);
-			memcpy(tzbuf, req_buf + sizeof(uint32_t),
+			memcpy_noio(tzbuf, req_buf + sizeof(uint32_t),
 				(sizeof(struct qseecom_key_generate_ireq) -
 				sizeof(uint32_t)));
 			dmac_flush_range(tzbuf, tzbuf + tzbuflen);
@@ -1048,7 +1048,7 @@ static int qseecom_scm_call2(uint32_t svc_id, uint32_t tz_cmd_id,
 			if (!tzbuf)
 				return -ENOMEM;
 			memset(tzbuf, 0, tzbuflen);
-			memcpy(tzbuf, req_buf + sizeof(uint32_t),
+			memcpy_noio(tzbuf, req_buf + sizeof(uint32_t),
 				(sizeof(struct qseecom_key_delete_ireq) -
 				sizeof(uint32_t)));
 			dmac_flush_range(tzbuf, tzbuf + tzbuflen);
@@ -1070,7 +1070,7 @@ static int qseecom_scm_call2(uint32_t svc_id, uint32_t tz_cmd_id,
 			if (!tzbuf)
 				return -ENOMEM;
 			memset(tzbuf, 0, tzbuflen);
-			memcpy(tzbuf, req_buf + sizeof(uint32_t),
+			memcpy_noio(tzbuf, req_buf + sizeof(uint32_t),
 				(sizeof(struct qseecom_key_select_ireq) -
 				sizeof(uint32_t)));
 			dmac_flush_range(tzbuf, tzbuf + tzbuflen);
@@ -1092,7 +1092,7 @@ static int qseecom_scm_call2(uint32_t svc_id, uint32_t tz_cmd_id,
 			if (!tzbuf)
 				return -ENOMEM;
 			memset(tzbuf, 0, tzbuflen);
-			memcpy(tzbuf, req_buf + sizeof(uint32_t), (sizeof
+			memcpy_noio(tzbuf, req_buf + sizeof(uint32_t), (sizeof
 				(struct qseecom_key_userinfo_update_ireq) -
 				sizeof(uint32_t)));
 			dmac_flush_range(tzbuf, tzbuf + tzbuflen);
