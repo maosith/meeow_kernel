3f74e2c589bed178cc377fc57a3827f96ec4fc9e
maosith <mns.ao@proton.me>
2023-04-29 15:55:25 -0300

    config gcc

--- build.sh
+++ build.sh
@@ -2,18 +2,18 @@
 
 make O=out ARCH=arm64 r8q_defconfig
 
-PATH=$PATH:/home/mao-server/Downloads/clang-r475365b/bin/
-
 make -j$(nproc --all) O=out \
                       ARCH=arm64 \
-                      CC=/home/mao-server/downloads/clang-r475365b/bin/clang \
-                      CLANG_TRIPLE=aarch64-linux-gnu- \
-                      CROSS_COMPILE=/home/mao-server/Downloads/gcc-linaro-7.4.1/gcc/bin/aarch64-linux-gnu- \
-                      r8q_defconfig
-
+                      CROSS_COMPILE=aarch64-linux-gnu- \
+                      
 make -j$(nproc --all) O=out \
                       ARCH=arm64 \
-                      CC=clang \
-                      CLANG_TRIPLE=aarch64-linux-gnu- \
-                      CROSS_COMPILE=/home/mao-server/Downloads/gcc-linaro-7.4.1/gcc/bin/aarch64-linux-gnu- \
-                      Image.gz
+                      CROSS_COMPILE=aarch64-linux-gnu- \
+                      
+if [[ -f "$Image" ]]; then
+    rm AnyKernel3/*.zip > /dev/null 2>&1
+    cp $Image AnyKernel3/Image.gz
+    cd AnyKernel3
+    zip -r9 meow-r8q.zip .
+fi
+
